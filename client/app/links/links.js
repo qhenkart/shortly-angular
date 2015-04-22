d99b96b984a96dc597d94f54ab1dadaa21e60717
qhenkart <qhenkart@gmail.com>
2015-04-22 15:31:47 -0700

    all tests working

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,12 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $http, Links) {
+  $scope.data = {};
+  $scope.getLinks = function(){
+    $http.get("/api/links")
+      .success(function(links){
+        $scope.data.links = links
+      });
+  }
+  $scope.getLinks();
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,12 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+.controller('ShortenController', function ($scope, $http, $location, Links) {
+  $scope.link = {};
+
+  $scope.addLink = function(){
+    $http.post('/api/links', $scope.link)
+      .success(function(data, status, headers, config){
+        console.log(data, status)
+      });
+  }
 });
